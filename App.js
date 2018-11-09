e7e312ffc639cdd7612ecd3a0f0340cd483a8cc1
AleksandarSavic95 <alek_bn@hotmail.com>
2018-11-09 09:49:06 +0100

    #ASLR-20 polje za dodavanje + props

--- App.js
+++ App.js
@@ -4,12 +4,40 @@ import {StyleSheet, Text, View} from 'react-native';
 import Header from './header';
 import Footer from './footer';
 
-type Props = {};
-export default class App extends Component<Props> {
+// type Props = {};
+export default class App extends Component { // <Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      items: []
+    }
+    this.handleAddItem = this.handleAddItem.bind(this);
+  }
+
+  handleAddItem() {
+    if (!this.state.value) return; // ignore empty strings
+    const newItems = [
+      ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete: false
+      }
+    ]
+    this.setState({
+      items: newItems,
+      value: ""
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header />
+        <Header
+          value={this.state.value}
+          onAddItem={this.handleAddItem}
+          onChange={(value) => this.setState({ value })}/>
         <View style={styles.content}>
           <Text>BUM!</Text>
         </View>
--- header.js
+++ header.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, TextInput } from 'react-native'
 
 export default class Header extends Component {
   render() {
     return (
-      <View>
-        <Text> Heder </Text>
+      <View style={styles.header}>
+        <TextInput
+          value={this.props.value}
+          onAddItem={this.handleAddItem}
+          onChangeText={this.props.onChange}
+          onSubmitEditing={this.props.onAddItem}
+          placeholder="What do you need to do?"
+          blurOnSubmit={false}
+          returnKeyType="done"
+          style={styles.input}/>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+    input: {
+        flex: 1,
+        height: 50
+    },
+    header: {
+        paddingHorizontal: 16,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+
+    }
+})
